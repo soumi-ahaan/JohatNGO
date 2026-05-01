cc768c5c6b63aed01163c1b00ce88a8ce95a95f0
cyber-s-tech <subhadeep.ahaansoftware@gmail.com>
2026-05-01 20:09:59 +0530

    images

--- src/Components/Pages/About/AboutComponents.jsx
+++ src/Components/Pages/About/AboutComponents.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import AboutBanner from '../About/AboutBanner';
+import AboutSection from '../About/AboutSection';
 import CallToAction from '../Home/CallToAction';
 import Testimonials from '../Home/Testimonials';
+import FAQ from '../About/FAQ'
 
 const AboutComponents = () => {
   return (
     <div>
+      <AboutBanner/>
+      <AboutSection/>
       <CallToAction/>
       <Testimonials/>
+      <FAQ/>
     </div>
   )
 }
