878e68445a9c86d82806d897b08326b0ef39abad
soumi-ahaan <soumi.k@ahaansoftwareconsulting.com>
2026-05-13 20:18:19 +0530

    updated margin

--- src/Components/Pages/Work/CounterSection.jsx
+++ src/Components/Pages/Work/CounterSection.jsx
@@ -38,7 +38,7 @@ const Counter = ({ end, suffix = "" }) => {
 const CounterSection = () => {
   return (
     <div className="relative z-20 mt-25 ">
-      <div className="absolute -top-[140px] md:-top-20 left-1/2 -translate-x-1/2 w-full max-w-[1440px] px-4 md:px-8 lg:px-12 xl:px-[72px]">
+      <div className="absolute -top-[168px] md:-top-20 left-1/2 -translate-x-1/2 w-full max-w-[1440px] px-4 md:px-8 lg:px-12 xl:px-[72px]">
         
         <div className="bg-[#FFAC00] rounded-2xl shadow-[0px_0px_10px_5px_#00000026] p-4 sm:p-6 md:py-8 md:px-6 grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 text-white text-center">
 
